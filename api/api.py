97aba78860abce7f178740e4ac48b977a5593c6d
Roy Glick <roy.glick@gmail.com>
2024-06-20 18:10:35 +0300

    start with vite

--- api/api.py
+++ api/api.py
@@ -1,19 +1,20 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
 def not_found(e):
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
 
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-
-@app.route('/api/time')
+@app.route("/api/time")
 def get_current_time():
-    return {'time': time.time()}
+    return {"time": time.time()}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
