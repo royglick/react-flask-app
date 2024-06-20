4e954f88fa6b345100739709340c431fbb4f4fbb
Roy Glick <roy.glick@gmail.com>
2024-06-20 11:44:41 +0300

    save before vite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +7,10 @@ function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
+    // fetch('/api/time').then(res => res.json()).then(data => {
+    //   setCurrentTime(data.time);
+    // });
+    fetch('/api/time ').then(res => console.log(res)).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
@@ -15,14 +18,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <div>
             <Link className="App-link" to="/">Home</Link>
             &nbsp;|&nbsp;
             <Link className="App-link" to="/page2">Page2</Link>
           </div>
-          <Switch>
+          <Routes>
             <Route exact path="/">
+              <React.Fragment>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                   Edit <code>src/App.js</code> and save to reload.
@@ -36,12 +40,13 @@ function App() {
                   Learn React
                 </a>
                 <p>The current time is {currentTime}.</p>
+              </React.Fragment>
             </Route>
             <Route path="/page2">
                 <p>This is page 2!</p>
             </Route>
-          </Switch>
-        </BrowserRouter>
+          </Routes>
+        </BrowserRouter> */}
       </header>
     </div>
   );
