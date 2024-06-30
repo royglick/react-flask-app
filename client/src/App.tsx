0bf68346c5a951d5b9cc1f60be0b9cec06f40fe4
Roy Glick <roy.glick@gmail.com>
2024-06-30 13:26:30 +0300

    add liquid letters

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+// import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import P5LiquidWords from "./p5js/P5LiquidWords/P5LiquidWords";
 
-
-switch(process.env.NODE_ENV) {
-  case 'production':
-    axios.defaults.baseURL = 'https://roy-glick.com';
+switch (process.env.NODE_ENV) {
+  case "production":
+    axios.defaults.baseURL = "https://roy-glick.com";
     break;
-  case 'development':
-    axios.defaults.baseURL = 'http://localhost:5001';
+  case "development":
+    axios.defaults.baseURL = "http://localhost:5001";
     break;
 }
 
 function App() {
-
   return (
-   <div className="App">
-    <div className="navbar">
-     <ul>
-      <li>Hi</li>
-     </ul>
+    <div className="App">
+      <div className="navbar">
+        <P5LiquidWords
+          width={window.innerWidth}
+          height={window.innerHeight}
+          word={"HELLO"}
+        />
+      </div>
     </div>
-   </div>
-  )
- }
- export default App
+  );
+}
+export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {viteRequire} from "vite-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(),viteRequire()],
   preview: {
    port: 80,
    strictPort: true,
